4d135fa44f40eae8743ed7a40ebd18d6d1719511
ffminus <ffminus@protonmail.com>
2025-01-24 00:37:18 +0100

    Store connection parameter in data source object

--- src/datasource.ts
+++ src/datasource.ts
@@ -10,8 +10,17 @@ import {
 import { Query, Options } from './types';
 
 export class DataSource extends DataSourceApi<Query, Options> {
+  /** URL of data source, proxied by server. */
+  readonly url: string;
+
+  /** Document to use as data source. */
+  readonly doc: string;
+
   constructor(instanceSettings: DataSourceInstanceSettings<Options>) {
     super(instanceSettings);
+
+    this.url = instanceSettings.url!;
+    this.doc = instanceSettings.jsonData.doc;
   }
 
   /** Check whether we can connect to the API. */
